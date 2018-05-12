e438d98e86e304a6352f71c7b495c310949d57c4
Matt Haddock <brothergomez@hotmail.com>
2018-05-12 14:05:04 +0100

    fixed the se_rec graph

--- send_rec_graph.py
+++ send_rec_graph.py
@@ -1,6 +1,6 @@
 """
 Load data from mongo and input into networkx directed graph, then output to gephi. This is 
-done on a limited subset of the enron emails, circa 2K emails
+done on a limited subset of the enron emails, circa 5K emails
 """
 
 import pymongo
@@ -10,6 +10,13 @@ import datetime
 import networkx as nx
 import pandas as pd
 
+
+def remove_domain(email):
+    email = email.split('.')
+    email = '.'.join(email[0: (len(email)-1)])
+    return email
+
+
 # connect to mongo
 cn = MongoClient('localhost')
 db = cn.enron_mail_2
@@ -17,13 +24,12 @@ G = nx.DiGraph()
 
 # counter to log progress
 counter = 1
-enron_emails = db.mail.find(
-    {"recipients": {"$ne": None}})
+enron_emails = db.mail.find({"folder": {"$regex": "^((?!sent).)*$"},"sender": {"$regex": "enron"}})
 
 # loop through all emails
 for document in enron_emails:
     # get rid of the stuff after @ as enron.com, net can confuse things
-    sender = document["sender"].split('@')[0]
+    sender = remove_domain(document["sender"])
     counter += 1
     # add 2 sets of weights, one for normal emails and one for messages of interest
     if document["message_of_interest"] == True:
@@ -37,7 +43,7 @@ for document in enron_emails:
 
     # loop over all recipients, doing the same thing
     for rec in document["recipients"]:
-        rec_split = rec.split('@')[0]
+        rec_split = remove_domain(rec)
         if G.has_edge(sender, rec_split):
             # we added this one before, just increase the weight by one
             G[sender][rec_split]['weight'] += 1
@@ -45,6 +51,10 @@ for document in enron_emails:
         else:
             # new edge. add with weight=1
             G.add_edge(sender, rec_split, weight=1, moi_weight=moi_weight)
-print("done writing edges")
+print("done writing graph G")
+print("nodes:", len(G.nodes))
+print("edges:", len(G.edges))
 # output to gephi
-nx.write_gexf(G, "enron.gexf")
+# print(G.nodes)
+
+nx.write_gexf(G, "enron_v2.gexf")
